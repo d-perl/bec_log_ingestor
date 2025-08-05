33f19866505d250e768ce7cbd41e8f1594087f71
perl_d <david.perl@psi.ch>
2025-08-05 16:22:10 +0200

    feat: bulk push to elastic

--- src/log_msg.rs
+++ src/log_msg.rs
@@ -1,72 +1,72 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, PartialEq, Deserialize, Serialize)]
-struct Elapsed {
-    repr: String,
-    seconds: f64,
+#[derive(Debug, PartialEq, Deserialize, Serialize, Clone)]
+pub struct Elapsed {
+    pub repr: String,
+    pub seconds: f64,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize, Clone)]
+pub struct File {
+    pub name: String,
+    pub path: String,
+}
+#[derive(Debug, PartialEq, Deserialize, Serialize, Clone)]
+pub struct LogLevel {
+    pub icon: String,
+    pub name: String,
+    pub no: usize,
+}
+#[derive(Debug, PartialEq, Deserialize, Serialize, Clone)]
+pub struct NameId {
+    pub name: String,
+    pub id: usize,
+}
+#[derive(Debug, PartialEq, Deserialize, Serialize, Clone)]
+pub struct Timestamp {
+    pub repr: String,
+    pub timestamp: f64,
+}
+#[derive(Debug, PartialEq, Deserialize, Serialize, Clone)]
+pub struct LogRecord {
+    pub elapsed: Elapsed,
+    pub exception: Option<serde_json::Value>,
+    pub extra: serde_json::Value,
+    pub file: File,
+    pub function: String,
+    pub level: LogLevel,
+    pub line: usize,
+    pub message: String,
+    pub module: String,
+    pub name: String,
+    pub process: NameId,
+    pub thread: NameId,
+    pub time: Timestamp,
 }
 
 #[derive(Debug, PartialEq, Deserialize, Serialize)]
-struct File {
-    name: String,
-    path: String,
-}
-#[derive(Debug, PartialEq, Deserialize, Serialize)]
-struct LogLevel {
-    icon: String,
-    name: String,
-    no: usize,
-}
-#[derive(Debug, PartialEq, Deserialize, Serialize)]
-struct NameId {
-    name: String,
-    id: usize,
-}
-#[derive(Debug, PartialEq, Deserialize, Serialize)]
-struct Timestamp {
-    repr: String,
-    timestamp: f64,
-}
-#[derive(Debug, PartialEq, Deserialize, Serialize)]
-struct LogRecord {
-    elapsed: Elapsed,
-    exception: Option<serde_json::Value>,
-    extra: serde_json::Value,
-    file: File,
-    function: String,
-    level: LogLevel,
-    line: usize,
-    message: String,
-    module: String,
-    name: String,
-    process: NameId,
-    thread: NameId,
-    time: Timestamp,
+pub struct LogMsg {
+    pub record: LogRecord,
+    pub service_name: String,
+    pub text: String,
 }
 
 #[derive(Debug, PartialEq, Deserialize, Serialize)]
-struct LogMsg {
-    record: LogRecord,
-    service_name: String,
-    text: String,
+pub struct LogMessage {
+    pub log_type: String,
+    pub log_msg: LogMsg,
+    pub metadata: serde_json::Value,
 }
 
 #[derive(Debug, PartialEq, Deserialize, Serialize)]
-struct LogMessage {
-    log_type: String,
-    log_msg: LogMsg,
-    metadata: serde_json::Value,
-}
-
-#[derive(Debug, PartialEq, Deserialize, Serialize)]
-struct LogMessagePackInternal {
-    encoder_name: String,
-    type_name: String,
-    data: LogMessage,
+pub struct LogMessagePackInternal {
+    pub encoder_name: String,
+    pub type_name: String,
+    pub data: LogMessage,
 }
 
 #[derive(Debug, PartialEq, Deserialize, Serialize)]
 pub struct LogMessagePack {
     #[serde(rename = "__bec_codec__")]
-    bec_codec: LogMessagePackInternal,
+    pub bec_codec: LogMessagePackInternal,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
-use std::error::Error;
-
+use elasticsearch::{
+    BulkParts, Elasticsearch,
+    http::{request::JsonBody, transport::Transport},
+};
 use redis::Commands;
 use rmp_serde;
+use serde_json::to_string;
+
+use std::{error::Error, iter::once};
+
+use crate::log_msg::LogRecord;
 mod log_msg;
 
 const LOGGING_ENDPOINT: [&str; 1] = ["info/log"];
 const KEY_MISMATCH: &str = "We got a response for request with one key, there must be one key!";
 const NO_DATA: &str = "Uh oh, log message contained no data";
+
+fn stream_read_opts() -> redis::streams::StreamReadOptions {
+    redis::streams::StreamReadOptions::default().count(5)
+}
 
 fn str_error(err: &str) -> Box<dyn Error> {
     Box::<dyn Error>::from(err)
@@ -17,23 +28,34 @@ fn redis_conn() -> Result<redis::Connection, redis::RedisError> {
     client.get_connection()
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let mut conn = redis_conn()?;
+fn elastic_client(api_key: &str) -> Result<Elasticsearch, elasticsearch::Error> {
+    let url = elasticsearch::http::Url::parse("http://localhost:9200")?;
+    let conn_pool = elasticsearch::http::transport::SingleNodeConnectionPool::new(url);
+    let credentials = elasticsearch::auth::Credentials::EncodedApiKey(api_key.into());
+    let transport = elasticsearch::http::transport::TransportBuilder::new(conn_pool)
+        .auth(credentials)
+        .build()?;
+    Ok(Elasticsearch::new(transport))
+}
 
-    let opts = redis::streams::StreamReadOptions::default().count(5);
-    let var: redis::streams::StreamReadReply =
-        conn.xread_options(&LOGGING_ENDPOINT, &[0], &opts)?;
+fn read_logs(redis_conn: &mut redis::Connection) -> Result<Vec<redis::Value>, Box<dyn Error>> {
+    let raw_reply: redis::streams::StreamReadReply =
+        redis_conn.xread_options(&LOGGING_ENDPOINT, &[0], &stream_read_opts())?;
 
-    let key = var.keys.get(0).ok_or_else(|| str_error(KEY_MISMATCH))?;
+    let log_key = raw_reply
+        .keys
+        .get(0)
+        .ok_or_else(|| str_error(KEY_MISMATCH))?;
 
-    let data: Vec<&redis::Value> = key
+    log_key
         .ids
         .iter()
-        .map(|e| e.map.get("data").ok_or_else(|| str_error(NO_DATA)))
-        .collect::<Result<Vec<&redis::Value>, Box<dyn Error>>>()?;
+        .map(|e| e.map.get("data").ok_or_else(|| str_error(NO_DATA)).cloned())
+        .collect::<Result<Vec<redis::Value>, Box<dyn Error>>>()
+}
 
-    let un_valued: Vec<Vec<u8>> = data
+fn process_data(values: Vec<redis::Value>) -> Result<Vec<log_msg::LogMessagePack>, Box<dyn Error>> {
+    let un_valued: Vec<Vec<u8>> = values
         .iter()
         .map(|e| match e {
             redis::Value::BulkString(x) => Ok(x.iter().cloned().collect()),
@@ -41,12 +63,74 @@ async fn main() -> Result<(), Box<dyn Error>> {
         })
         .collect::<Result<Vec<Vec<u8>>, Box<dyn Error>>>()?;
 
-    let un_mspacked: Vec<log_msg::LogMessagePack> = un_valued
+    Ok(un_valued
         .iter()
         .map(|e| rmp_serde::from_slice::<log_msg::LogMessagePack>(e))
-        .collect::<Result<Vec<log_msg::LogMessagePack>, rmp_serde::decode::Error>>()?;
+        .collect::<Result<Vec<log_msg::LogMessagePack>, rmp_serde::decode::Error>>()?)
+}
 
-    dbg!(un_mspacked);
+fn extract_records(messages: Vec<log_msg::LogMessagePack>) -> Vec<log_msg::LogRecord> {
+    messages
+        .iter()
+        .map(|e| e.bec_codec.data.log_msg.record.clone())
+        .collect()
+}
+
+fn make_index_action_body(records: &Vec<LogRecord>) -> Result<String, serde_json::Error> {
+    let action = serde_json::json!({ "index": { "_index": "test-index" } });
+    let body_vec: Vec<serde_json::Value> = records
+        .iter()
+        .flat_map(|e| once(Ok(action.clone())).chain(once(serde_json::to_value(&e))))
+        .collect::<Result<Vec<serde_json::Value>, serde_json::Error>>()?;
+    Ok(body_vec
+        .iter()
+        .map(|e| to_string(e))
+        .collect::<Result<Vec<String>, serde_json::Error>>()?
+        .join("\n"))
+}
+
+fn make_docs_values(
+    records: &Vec<LogRecord>,
+) -> Result<Vec<JsonBody<serde_json::Value>>, serde_json::Error> {
+    let action = serde_json::json!({ "create": {} });
+
+    let values = records
+        .iter()
+        .map(|e| serde_json::to_value(e))
+        .collect::<Result<Vec<serde_json::Value>, serde_json::Error>>()?;
+
+    Ok(values
+        .iter()
+        .flat_map(|e| {
+            once(JsonBody::from(action.clone())).chain(once(JsonBody::from(e.to_owned())))
+        })
+        .collect())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let mut redis_conn = redis_conn()?;
+    let elastic_client =
+        elastic_client("ZUl6dGVaZ0JEbkl1Njd5RGl2MVc6UUluVTVWZ0FqNDZ2VlQzYkRwRHJzQQ==")?;
+
+    // get logs data from redis and convert to record structs
+    let data = read_logs(&mut redis_conn)?;
+    let unpacked = process_data(data)?;
+    let records = extract_records(unpacked);
+
+    dbg!(&records);
+
+    let body = make_docs_values(&records)?;
+    // Send bulk request
+    let response = elastic_client
+        .bulk(BulkParts::Index("test-index"))
+        .body(body)
+        .send()
+        .await?;
+
+    // Optional: check response
+    let response_body = response.json::<serde_json::Value>().await?;
+    println!("{:#}", response_body);
 
     Ok(())
 }
